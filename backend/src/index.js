73b0a23fb3d68254f7654d4a5a91d1de05dc8fc2
Julianne <julivolotao@gmail.com>
2020-03-29 00:25:24 -0300

    Imagens readme, validações dos campos.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 // segurança, define quem vai ter acesso
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
@@ -8,4 +9,6 @@ const app = express();
 app.use(cors())
 app.use(express.json()); // para converter algo em json entendível para a aplicação
 app.use(routes)
+app.use(errors())
+
 app.listen(3333);
